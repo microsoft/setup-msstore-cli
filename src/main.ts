a9d09ab163a4ca0d021ffd7e004e25d7b5695d33
Alexandre Zollinger Chohfi <alzollin@microsoft.com>
2023-03-21 18:28:26 -0700

    Added ADO task.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,43 @@
 import * as core from '@actions/core'
+import * as tc from '@actions/tool-cache'
+import * as exec from '@actions/exec'
+import * as io from '@actions/io'
 import * as msstoreconfigurator from './msstoreconfigurator'
+
+const Version = 'version'
+
+class GitHubPipeline implements msstoreconfigurator.IPipeline {
+  debug(message: string): void {
+    core.debug(message)
+  }
+  addPath(p: string): void {
+    core.addPath(p)
+  }
+  async mkdirP(p: string): Promise<void> {
+    return io.mkdirP(p)
+  }
+  async downloadTool(url: string): Promise<string> {
+    return tc.downloadTool(url)
+  }
+  async extractTar(archivePath: string, dest: string): Promise<string> {
+    return tc.extractTar(archivePath, dest)
+  }
+  async extractZip(archivePath: string, dest: string): Promise<string> {
+    return tc.extractZip(archivePath, dest)
+  }
+  async rmRF(p: string): Promise<void> {
+    return io.rmRF(p)
+  }
+  async exec(command: string, args: string[]): Promise<number> {
+    return exec.exec(command, args)
+  }
+}
 
 async function run(): Promise<void> {
   try {
-    await msstoreconfigurator.getConfig().configure()
+    await msstoreconfigurator
+      .getConfig(core.getInput(Version))
+      .configure(new GitHubPipeline())
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
